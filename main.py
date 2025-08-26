25593d8c42eeb832441b45d12f9eb8beec5bbb17
blackver69 <46539728+blackver69@users.noreply.github.com>
2025-08-26 16:48:40 +1000

    init: create folder structure

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from tkinter import *
+from tkinter import ttk
 
-class Main():
+from strings import en as STRINGS
+
+class Main:
     def __init__(self):
-        pass
+        self.root = Tk() 
+
+    def main_view(self):
+        frm = ttk.Frame(self.root, padding=100)
+        frm.grid()
+        ttk.Label(frm, text= STRINGS.APP_TITLE).grid(column=0, row=0)
+        self.root.mainloop()
+
+main = Main()
+main.main_view()
---
+++ models/user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self):
+        name: str
+        email: str
+        password: str
