9e8ee2cbb6822a3cea7b0aa20a4af58f798aaf62
blackver69 <46539728+blackver69@users.noreply.github.com>
2025-09-01 11:33:20 +1000

    update: update parameter

--- parameters/parameters.py
+++ parameters/parameters.py
@@ -1,1 +1,17 @@
-TEXT_INPUT = {    "height": 1,    "width": 40}TEXT = {    "small": 12,    "medium": 16,    "large": 24,    "lines_spacing": 2}BUTTON = {    "small": {4,4},    "medium": {8,8},    "large": {16,16}}
+TEXT_INPUT = {
+    "height": 1,
+    "width": 40
+}
+
+TEXT = {
+    "small": 12,
+    "medium": 16,
+    "large": 24,
+    "lines_spacing": 2
+}
+
+BUTTON = {
+    "small": {4,4},
+    "medium": {8,8},
+    "large": {16,16}
+}
